922ee4da13b2c76c183252e5e5f582b0092b30d4
Jude <lovespictures2@gmail.com>
2023-01-02 15:06:56 +0000

    Updated navigation icons

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import HomeIcon from "@material-ui/icons/Home";
-import PersonIcon from "@material-ui/icons/Person";
-import MapIcon from "@material-ui/icons/Map";
-import BubbleChartIcon from "@material-ui/icons/BubbleChart";
-import CollectionsBookmark from "@material-ui/icons/CollectionsBookmark";
-import AlternateEmailIcon from "@material-ui/icons/AlternateEmail";
+// import HomeIcon from "@material-ui/icons/Home";
+// import PersonIcon from "@material-ui/icons/Person";
+// import MapIcon from "@material-ui/icons/Map";
+// import BubbleChartIcon from "@material-ui/icons/BubbleChart";
+// import CollectionsBookmark from "@material-ui/icons/CollectionsBookmark";
+// import AlternateEmailIcon from "@material-ui/icons/AlternateEmail";
+// import { Icon } from "@material-ui/core";
 
 // import icons from icons
-// import Icons from "../img/Icons";
+import Icons from "../img/Icons";
 
 import "./Navbar.css";
-// import { Icon } from "@material-ui/core";
 
 const Navbar = () => {
   return (
@@ -20,27 +20,44 @@ const Navbar = () => {
       <ul>
         <li className="Primary__navbar-link">
           <Link to="/">
-            <HomeIcon alt="Home" />
+            {/* <HomeIcon alt="Home" /> */}
+            <img className="Nav__icon" src={Icons.iconHome} alt="Home icon" />
           </Link>
 
           <Link to="/about">
-            <PersonIcon alt="About" />
+            {/* <PersonIcon alt="About" />
+             */}
+            <img className="Nav__icon" src={Icons.iconPerson} alt="Map icon" />
           </Link>
 
           <Link to="/mapData">
-            <MapIcon alt="Map data" />
+            {/* <MapIcon alt="Map" />
+             */}
+            <img className="Nav__icon" src={Icons.iconMap} alt="Person icon" />
           </Link>
 
           <Link to="/Posters">
-            <BubbleChartIcon alt="Digital Posters" />
+            {/* <BubbleChartIcon alt="Digital Posters" /> */}
+            <img className="Nav__icon" src={Icons.iconCity} alt="Poster icon" />
           </Link>
 
           <Link to="/Gallery">
-            <CollectionsBookmark alt="Collections" />
+            {/* <CollectionsBookmark alt="Collections" />
+             */}
+            <img
+              className="Nav__icon"
+              src={Icons.iconGallery}
+              alt="Gallery icon"
+            />
           </Link>
 
           <Link to="/contact">
-            <AlternateEmailIcon />
+            <img
+              className="Nav__icon"
+              src={Icons.iconEmail}
+              alt="Gallery icon"
+            />
+            {/* <AlternateEmailIcon /> */}
           </Link>
         </li>
       </ul>
