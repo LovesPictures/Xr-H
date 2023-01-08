ab235a05fc9d5bfc8e4cb0d485ee369807b31cc7
Jude <lovespictures2@gmail.com>
2023-01-08 17:18:21 +0000

    included popups

--- src/components/Pages/Map/MapData.jsx
+++ src/components/Pages/Map/MapData.jsx
@@ -1,7 +1,12 @@
 //original  page - working on .map()
 import * as React from "react";
 import "mapbox-gl/dist/mapbox-gl.css";
-import Map, { Marker, GeolocateControl, NavigationControl } from "react-map-gl";
+import Map, {
+  Marker,
+  GeolocateControl,
+  NavigationControl,
+  Popup,
+} from "react-map-gl";
 // import GoringHall_01 from "../../img/GoringHall_01.png";
 import "./MapData.css";
 import { locationData } from "./locationData";
@@ -10,6 +15,9 @@ const MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoianVkaXRocmlja2V0dHMiLCJhIjoiY2w1djA4Y2FuMDR6OTNibnAzbm1oeWN4dCJ9.bwVGnCWwW0QgGJp1KAoN2Q";
 
 export const MapData = () => {
+  const [showPopup, setShowPopup] = React.useState(false);
+  console.log("showPopup " + showPopup);
+
   const data = locationData;
 
   return (
@@ -30,25 +38,30 @@ export const MapData = () => {
         style={{ width: "100%", height: "100%" }}
         mapStyle="mapbox://styles/mapbox/navigation-night-v1"
       >
+        {showPopup && (
+          <Popup
+            latitude={showPopup.latitude}
+            longitude={showPopup.longitude}
+            anchor="bottom"
+            onClose={() => setShowPopup(false)}
+          >
+            {showPopup.FirstName_LastName_UK}
+
+            {showPopup.people_enslaved}
+            {showPopup.compensation}
+          </Popup>
+        )}
         <GeolocateControl
           positionOptions={{ enableHighAccuracy: true }}
           trackUserLocation={true}
         />
-
-        {/* .map()
-    id: 1,
-    FirstName_LastName_UK: "Michael Nihell Bovell",
-    people_enslaved: 158,
-    compensation: 838,    £
-    latitude: 50.813492,
-    longitude: -0.372458, */}
 
         {data.map((pin) => {
           return (
             <Marker
               onClick={(e) => {
                 e.originalEvent.stopPropagation();
-                setShowPopup(true);
+                setShowPopup(pin);
               }}
               key={pin.id}
               latitude={pin.latitude}
